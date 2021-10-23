5b1501c61fb6db8bff1c168141afe2f5b60532f2
Ignacio Rivas <nachoiar@gmail.com>
2021-10-23 19:44:04 -0300

    Agreo funcion de jugador aleatorio

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ const fecha = new Date();
 boton_uno.addEventListener('click', () => {
     alert(fecha);
 })
+
+let jugadores =["Mesi", "Ronaldo", "Mbape"];
+ 
+let btn = document.querySelector("#btn-jugador");
+btn.addEventListener("click", sortearjugador);
+
+
+function sortearjugador(){
+  num = Math.floor(Math.random()*(jugadores.length));
+    console.log(jugadores[num]);      
+}
